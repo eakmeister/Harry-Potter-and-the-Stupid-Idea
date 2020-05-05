e30d69aae0b9d8777139fc4922589754125c7f8d
Eric Kedaigle <ekedaigle@mitre.org>
2020-05-05 17:08:53 -0400

    Initial commit

---
+++ harry_potter_and_the_stupid_idea.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name     Harry Potter and the Stupid Google Scholar Script
+// @version  1
+// @grant    none
+// @include https://scholar.google.com/scholar?*
+// ==/UserScript==
+
+console.log("here")
+var h3s = document.getElementsByClassName("gs_rt")
+
+
+for (var i = 0; i < h3s.length; i++) {
+  var h3 = h3s[i]
+  
+  var children = h3.children
+  for (var j = 0; j < children.length; j++) {
+    var child = children[j]
+		
+    if (child.tagName == "A") {
+      if (child.text.startsWith("The")) {
+        child.text = "Harry Potter and " + child.text
+      } else {
+    		child.text = "Harry Potter and the " + child.text
+      }
+    }
+  }
+}
